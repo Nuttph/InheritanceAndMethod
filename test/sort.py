f23bb4116aa5f31282056de2901946c5b7dc5788
Nuttaphon <131658916+Nuttph@users.noreply.github.com>
2024-09-10 22:30:06 +0700

    saving

--- test/sort.py
+++ test/sort.py
@@ -1,12 +1,7 @@
-#Ez sort
-
-print("Enter Number of List: ")
-l = list(map(int, input().split()))
+l = list(map(int,input().split()))
 for i in range(0,len(l)):
     for j in range(i+1,len(l)):
-        if l[i] <= l[j]:
+        if l[i] >= l[j]:
             l[i],l[j] = l[j],l[i]
 
-# print(l)
-for i in range(len(l)):
-    print(f'{i+1} : {l[i]}')
+print(l)
